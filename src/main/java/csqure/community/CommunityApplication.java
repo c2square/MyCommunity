cafe6ab58b45d1a1726681255204fc0239ca2a16
csqure <544366139@qq.com>
2020-04-08 16:19:29 +0800

    init repository

---
+++ src/main/java/csqure/community/CommunityApplication.java
@@ -0,0 +1,16 @@
+package csqure.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author csqure
+ */
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
